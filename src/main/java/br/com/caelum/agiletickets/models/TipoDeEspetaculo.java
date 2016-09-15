ddfa876baf203e2975caedd3d14db7069d51876a
moacir <mujolo@gmail.com>
2016-09-15 10:17:15 -0300

    Refarorando outras variaves

--- src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
+++ src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
@@ -9,13 +9,16 @@ public enum TipoDeEspetaculo {
 	
 	public static BigDecimal calculaPrecoSessaoCinemaShow(Sessao sessao) {
 		BigDecimal preco = sessao.getPreco();
-		if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.CINEMA) || sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.SHOW)) {
+		final Integer totalIngressos = sessao.getTotalIngressos();
+		final Integer ingressosReservados = sessao.getIngressosReservados();
+		final TipoDeEspetaculo tipo = sessao.getEspetaculo().getTipo();
+		if (tipo.equals(TipoDeEspetaculo.CINEMA) || tipo.equals(TipoDeEspetaculo.SHOW)) {
 			//quando estiver acabando os ingressos... 
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.05) { 
+			if((totalIngressos - ingressosReservados) / totalIngressos.doubleValue() <= 0.05) { 
 				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
 			}
-		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.BALLET) || sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA)) {
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.50) { 
+		} else if(tipo.equals(TipoDeEspetaculo.BALLET) || tipo.equals(TipoDeEspetaculo.ORQUESTRA)) {
+			if((totalIngressos - ingressosReservados) / totalIngressos.doubleValue() <= 0.50) { 
 				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
 			}
 			
